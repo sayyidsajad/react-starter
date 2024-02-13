408b1a4b43564ba72eef87354ccfb95801d0f04a
sayyidsajad <sayyidsajada4s@gmail.com>
2024-02-13 21:56:49 +0530

    React use state working

--- W3 Schools/my-react-app/src/index.js
+++ W3 Schools/my-react-app/src/index.js
@@ -1,26 +1,23 @@
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Todos from "./Todos";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
-
-  const increment = () => {
-    setCount((c) => c + 1);
-  };
+function Car() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
 
   return (
     <>
-      <Todos todos={todos} />
-      <hr />
-      <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
-      </div>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
     </>
-  );
-};
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Car />);
